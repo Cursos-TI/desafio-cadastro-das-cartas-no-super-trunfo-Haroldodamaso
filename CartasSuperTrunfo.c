51a9f30d71f611af560b1e02897de73169cbab40
Haroldodamaso <haroldodamasso@hotmail.com>
2025-08-06 20:30:53 +0000

    criei o printf novTO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
-}
+    // declaração das variáveis
+    int opcaoEscolhida = 0, opcaoEscolhida2 = 0;
+    int somaComparacao1 = 0, somaComparacao2 = 0;
+    char estado1 = 'A', estado2 = 'B';
+    char codigoCarta1[10] = "A01", codigoCarta2[10] = "B01";
+    char nomeCidade1[20] = "São Paulo", nomeCidade2[20] = "Rio de Janeiro";
+    unsigned long int populacao1 = 12325000, populacao2 = 6748000;
+    float area1 = 1521.11, area2 = 1200.25;
+    float pib1 = 699.28, pib2 = 300.50;
+    int numPontosTuristicos1 = 50, numPontosTuristicos2 = 30;
+    double densidade1 = (double) populacao1 / area1, densidade2 = (double) populacao2 / area2;
+    double pibPerCapita1 = (double) pib1 * 1000000000 / populacao1, pibPerCapita2 = (double) pib2 * 1000000000 / populacao2;
+
+
+
+            }
